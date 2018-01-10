840bf4150b7dfad4ecd9edb998ac2c05f158eace
Bruk <bruk@updconsulting.com>
2018-01-10 05:48:45 -0500

    Added all packages and utility scripts

---
+++ Ed-Fi/interchange/utility/BulkLoadAll.ps1
@@ -0,0 +1,38 @@
+﻿
+$Directory = "$PSScriptRoot\..\xml"
+
+$LoadOrder =@("Descriptors",
+ "EducationOrganization","EducationOrgCalendar",
+ "MasterSchedule", "StaffAssociation" , 
+ "TeacherCandidate-Credential", "TeacherCandidate-Student",
+ "TeacherCandidate-TeacherCandidate","TeacherCandidate-TeacherCandidateAcademicRecord",
+ "TeacherCandidate-TeacherCandidateFieldworkExperience","TeacherCandidate-TeacherCandidateTeacherPreparationProviderAssociation",
+ "TeacherCandidate-TeacherCandidateTeacherPreparationProviderProgramAssociation", 
+ "PerformanceMeasure-PerformanceMeasure","PerformanceMeasure-Rubric", "PerformanceMeasure-RubricLevel","PerformanceMeasure-RubricLevelResponse", 
+ "AssessmentMetadata-EssentialSkills","StudentAssessment-EssentialSkills"
+ );
+ $FileOrder = @{};
+ foreach( $file in Get-ChildItem $Directory)
+ {
+   $re = [regex]'(_|-)\d+\.xml$'
+   $IndexFromTheEnd = if($re.Match($file).Success) { $re.Match($file).Value.Length } else { '.xml'.Length };
+   $FileName =   $file.Name;
+   $file = $file.Name.Substring('Interchnage-'.Length)
+   $file = $file.Substring(0, $file.Length - $IndexFromTheEnd)
+
+   $FileOrder.add(  $FileName , [array]::IndexOf($LoadOrder, $file));
+ }
+
+  
+  $FileOrder = $FileOrder.GetEnumerator() | Sort-Object -Property Value
+
+ foreach($file  in $FileOrder)
+ {
+   
+     $command = "$PSScriptRoot\BulkLoadFile.ps1"
+
+    
+
+    . $command -file $file.Key;      
+
+ }
---
+++ Ed-Fi/interchange/utility/RemoveGeneratedXML.ps1
@@ -0,0 +1,13 @@
+﻿
+$Directory = "$PSScriptRoot\..\xml"
+
+ foreach( $file in Get-ChildItem $Directory)
+ {
+   $re = [regex]'Descriptors|Assessment'
+    if( !$re.Match($file).Success) {
+   
+          Remove-Item $file
+          Write-Host $file
+   }
+   
+ }
